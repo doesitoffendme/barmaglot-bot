cee403933b461c2701e0a72be3870b6759929d81
WilixLead <lead@w.wilix.ru>
2018-03-18 15:19:52 +0300

    tempory remove url from news

--- bot_functions.php
+++ bot_functions.php
@@ -120,7 +120,7 @@ switch($message) {
         $news_string = "";
         if ($news) {
           foreach ($news['documents'] as $new) {
-            $news_string .= $new['title'] . "\n";
+            $news_string .= $new['title'];
           }
           sendMessage($chat_id, $news_string);
         } else {
